03968af70fdea6f42fc0b5eadec0e01e23ab09b6
Ashj12683 <49860840+Ashj12683@users.noreply.github.com>
2023-03-27 04:58:15 +0530

    this is portfolio website in react
    my fav react!

---
+++ src/components/Works/Works.jsx
@@ -0,0 +1,80 @@
+import React, { useContext } from "react";
+import "./Works.css";
+import PHPMysql from "../../img/phpMysql.png";
+import Woo from "../../img/woo.png";
+import Wordpress from "../../img/wp.png";
+import Shopify from "../../img/shopify.jpeg";
+import Reactimg from "../../img/Reactimg.png";
+import { themeContext } from "../../Context";
+import { motion } from "framer-motion";
+import {Link} from 'react-scroll'
+const Works = () => {
+  // context
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+
+  // transition
+  return (
+    <div className="works" id="works">
+      {/* left side */}
+      <div className="w-left">
+        <div className="awesome">
+          {/* dark Mode */}
+          <span style={{ color: darkMode ? "white" : "" }}>
+            Works for All these
+          </span>
+          <span>Brands & Clients</span>
+          <spane>
+            Lorem ispum is simpley dummy text of printing of printing Lorem
+            <br />
+            ispum is simpley dummy text of printingLorem ispum is simpley dummy
+            text
+            <br />
+            y dummy text of printingLorem
+            <br />
+            ispum is simpley dummy text of printing
+          </spane>
+          <Link to="contact" smooth={true} spy={true}>
+            <button className="button s-button">Hire Me</button>
+          </Link>
+          <div
+            className="blur s-blur1"
+            style={{ background: "#ABF1FF94" }}
+          ></div>
+        </div>
+
+        {/* right side */}
+      </div>
+      <div className="w-right">
+        <motion.div
+          initial={{ rotate: 45 }}
+          whileInView={{ rotate: 0 }}
+          viewport={{ margin: "-40px" }}
+          transition={{ duration: 3.5, type: "spring" }}
+          className="w-mainCircle"
+        >
+          <div className="w-secCircle">
+            <img src={Wordpress} alt="" />
+          </div>
+          <div className="w-secCircle">
+            <img src={Woo} alt="" />
+          </div>
+          <div className="w-secCircle">
+            <img src={PHPMysql} alt="" />
+          </div>{" "}
+          <div className="w-secCircle">
+            <img src={Shopify} alt="" />
+          </div>
+          <div className="w-secCircle">
+            <img src={Reactimg} alt="" />
+          </div>
+        </motion.div>
+        {/* background Circles */}
+        <div className="w-backCircle blueCircle"></div>
+        <div className="w-backCircle yellowCircle"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Works;
